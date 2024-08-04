67412f8ad6e15ad14c1badd8e85532139823b50e
krishna <krishnasharmag30@gmail.com>
2024-08-04 16:13:37 +0530

    to-do-website

--- to-do-website/index.js
+++ to-do-website/index.js
@@ -1,41 +1,10 @@
-let inputText = document.getElementById("input-text");
 
-let addBtn = document.getElementById("add-btn");
-
-addBtn.addEventListener("click", addtask);
-
-function addtask() {
-  let inputText = document.getElementById("input-text").value;
-  console.log(inputText);
+// username 
+// adduser
 
 
-  let todoUl = document.getElementById("task-list");
-  console.log(todoUl);
+ let  addUserbtn = document.getElementById('adduser');
+ console.log(addUserbtn)
+ let UserInputname = document.getElementById('username')
 
-//   list create 
-  let listoftodos = document.createElement("li");
-
-  let todolist = listoftodos.textContent = inputText;
-
-  todoUl.append(todolist);
-
-  document.getElementById("input-text").value = " ";
-
-
-//  create btn 
-
-let tododiv = document.getElementById("new-task-list");
-console.log(tododiv);
-let editbtn = document.createElement("button")
-
- editbtn.textContent = "edit"; 
- tododiv.append(editbtn)
-let deletebtn = document.createElement("button")
- deletebtn.textContent = "delete"
- tododiv.append(deletebtn);
- deletebtn.onclick = function(){
-    tododiv.removeChild()
- }
-
-
-}
+ console.log(UserInputname.value)
